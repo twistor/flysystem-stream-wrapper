188f70c915cc3ceee2c50837cc5953d8aab9a38a
Chris Leppanen <chris.leppanen@gmail.com>
2015-05-18 14:46:32 -0700

    Clean up tests.

--- tests/FlysystemStreamWrapperTest.php
+++ tests/FlysystemStreamWrapperTest.php
@@ -11,6 +11,8 @@ class FlysystemStreamWrapperTest extends \PHPUnit_Framework_TestCase
 {
     protected $testDir;
 
+    protected $filesystem;
+
     public function setUp()
     {
         $this->testDir = __DIR__ . '/testdir';
@@ -18,20 +20,15 @@ class FlysystemStreamWrapperTest extends \PHPUnit_Framework_TestCase
         $filesystem = new Filesystem(new Local(__DIR__));
         $filesystem->deleteDir('testdir');
         $filesystem->createDir('testdir');
+
+        $this->filesystem = new Filesystem(new Local($this->testDir));
+        FlysystemStreamWrapper::register('flysystem', $this->filesystem);
     }
 
     public function tearDown()
     {
         $filesystem = new Filesystem(new Local(__DIR__));
         $filesystem->deleteDir('testdir');
-    }
-
-    protected function getFilesystem()
-    {
-        $filesystem = new Filesystem(new Local($this->testDir));
-        FlysystemStreamWrapper::register('flysystem', $filesystem);
-
-        return $filesystem;
     }
 
     public function testRegister()
@@ -50,21 +47,20 @@ class FlysystemStreamWrapperTest extends \PHPUnit_Framework_TestCase
 
     public function testMeta()
     {
-        $filesystem = $this->getFilesystem();
-        $this->assertSame(file_put_contents('flysystem://test_file.txt', 'contents'), 8);
+        $this->putContent('test_file.txt', 'contents');
 
-        $this->assertTrue(file_exists('flysystem://test_file.txt'));
-
-        $this->assertFalse(file_exists('flysystem://not_exist'));
+        $this->assertWrapperFileNotExists('not_exist');
 
         $this->assertTrue(mkdir('flysystem://test_dir'));
         $this->assertTrue(is_dir('flysystem://test_dir'));
 
         // Test touch.
         $this->assertTrue(touch('flysystem://touched'));
-        $this->assertTrue(file_exists($this->testDir . '/touched'));
+        $this->assertFileContent('touched', '');
+
+        // Test touching an existing file.
         $this->assertTrue(touch('flysystem://test_file.txt'));
-        $this->assertTrue(file_exists($this->testDir . '/test_file.txt'));
+        $this->assertFileContent('test_file.txt', 'contents');
 
         // Chown isn't supported.
         $this->assertFalse(chown('flysystem://touched', 'asfasf'));
@@ -73,47 +69,35 @@ class FlysystemStreamWrapperTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue(chmod('flysystem://touched', 0777));
     }
 
-    public function testWrite()
+    public function testUnlink()
     {
-        $filesystem = $this->getFilesystem();
+        $this->putContent('test_file.txt', 'contents');
 
-        file_put_contents('flysystem://test_file.txt', 'contents');
-        $this->assertSame(file_get_contents($this->testDir . '/test_file.txt'), 'contents');
-        $this->assertSame(file_get_contents('flysystem://test_file.txt'), 'contents');
-
-        // Test unlink.
         $this->assertTrue(unlink('flysystem://test_file.txt'));
+        $this->assertWrapperFileNotExists('test_file.txt');
     }
 
     public function testRename()
     {
-        $filesystem = $this->getFilesystem();
+        $this->putContent('test_file.txt', 'contents');
 
-        file_put_contents('flysystem://test_file.txt', 'contents');
-
+        // Test rename to same file.
         rename('flysystem://test_file.txt', 'flysystem://test_file.txt');
-        $this->assertSame(file_get_contents('flysystem://test_file.txt'), 'contents');
+        $this->assertFileContent('test_file.txt', 'contents');
 
         rename('flysystem://test_file.txt', 'flysystem://test_file2.txt');
-        $this->assertSame(file_get_contents('flysystem://test_file2.txt'), 'contents');
+        $this->assertFileContent('test_file2.txt', 'contents');
+        $this->assertWrapperFileNotExists('test_file.txt');
 
-        // Test overwriting existing files.
-        file_put_contents('flysystem://test_file3.txt', '');
+        // // Test overwriting existing files.
+        $this->putContent('test_file3.txt', 'oops');
         rename('flysystem://test_file2.txt', 'flysystem://test_file3.txt');
-    }
-
-    /**
-     * @expectedException PHPUnit_Framework_Error_Warning
-     */
-    public function testBadRename()
-    {
-        $filesystem = $this->getFilesystem();
-        rename('flysystem://test_file1.txt', 'flysystem://test_file3.txt');
+        $this->assertWrapperFileNotExists('test_file2.txt');
+        $this->assertFileContent('test_file3.txt', 'contents');
     }
 
     public function testRmdir()
     {
-        $filesystem = $this->getFilesystem();
         mkdir($this->testDir . '/bad');
 
         chmod($this->testDir . '/bad', 0000);
@@ -125,43 +109,22 @@ class FlysystemStreamWrapperTest extends \PHPUnit_Framework_TestCase
 
     public function testTruncateTellAndSeek()
     {
-        $filesystem = $this->getFilesystem();
-
-        file_put_contents('flysystem://test_file.txt', 'contents');
+        $this->putContent('test_file.txt', 'contents');
 
         $handle = fopen('flysystem://test_file.txt', 'r+');
 
         fseek($handle, 1);
 
-        $this->assertSame(ftell($handle), 1);
+        $this->assertSame(1, ftell($handle));
 
         ftruncate($handle, 0);
         fclose($handle);
-        $this->assertSame(file_get_contents('flysystem://test_file.txt'), '');
-    }
-
-    /**
-     * @expectedException PHPUnit_Framework_Error_Warning
-     */
-    public function testFailedRmdir()
-    {
-        $filesystem = $this->getFilesystem();
-        rmdir('flysystem://');
-    }
-
-    /**
-     * @expectedException PHPUnit_Framework_Error_Warning
-     */
-    public function testFailedUnlink()
-    {
-        $filesystem = $this->getFilesystem();
-        unlink('flysystem://asdfasdfasf');
+        $this->assertFileContent('test_file.txt', '');
     }
 
     public function testLock()
     {
-        $filesystem = $this->getFilesystem();
-        $handle = fopen('flysystem://file', 'r+');
+        $handle = fopen('flysystem://file', 'w');
 
         // HHVM allows locks on memory handles?
         if (defined('HHVM_VERSION')) {
@@ -173,9 +136,7 @@ class FlysystemStreamWrapperTest extends \PHPUnit_Framework_TestCase
 
     public function testSetOption()
     {
-        $filesystem = $this->getFilesystem();
-
-        $handle = fopen('flysystem://thing', 'r+');
+        $handle = fopen('flysystem://thing', 'w+');
 
         $this->assertTrue(stream_set_blocking($handle, 0));
         $this->assertFalse(stream_set_timeout($handle, 10));
@@ -186,8 +147,6 @@ class FlysystemStreamWrapperTest extends \PHPUnit_Framework_TestCase
 
     public function testDirectoryIteration()
     {
-        $filesystem = $this->getFilesystem();
-
         mkdir('flysystem://one');
         mkdir('flysystem://two');
         mkdir('flysystem://three');
@@ -207,14 +166,72 @@ class FlysystemStreamWrapperTest extends \PHPUnit_Framework_TestCase
 
     public function testSelect()
     {
-        $filesystem = $this->getFilesystem();
-
-        $handle = fopen('flysystem://thing', 'r+', TRUE);
+        $handle = fopen('flysystem://thing', 'w+', TRUE);
 
         $read = [$handle];
         $write = null;
         $except = null;
         $this->assertSame(stream_select($read, $write, $except, 10), 1);
         fclose($handle);
+    }
+
+    public function testWriteEmptyFile() {
+        $this->putContent('file', '');
+        $this->assertWrapperFileExists('file');
+    }
+
+    /**
+     * @expectedException PHPUnit_Framework_Error_Warning
+     */
+    public function testFailedRmdir()
+    {
+        rmdir('flysystem://');
+    }
+
+    /**
+     * @expectedException PHPUnit_Framework_Error_Warning
+     */
+    public function testFailedUnlink()
+    {
+        unlink('flysystem://asdfasdfasf');
+    }
+
+    /**
+     * @expectedException PHPUnit_Framework_Error_Warning
+     */
+    public function testBadRename()
+    {
+        rename('flysystem://test_file1.txt', 'flysystem://test_file3.txt');
+    }
+
+    /**
+     * @expectedException PHPUnit_Framework_Error_Warning
+     */
+    public function testReadMissing() {
+        fopen('flysystem://doesnotexist', 'rbt');
+    }
+
+    protected function assertFileContent($path, $content)
+    {
+        $this->assertSame($content, file_get_contents("flysystem://$path"));
+        $this->assertSame($content, file_get_contents($this->testDir . "/$path"));
+    }
+
+    protected function assertWrapperFileExists($path)
+    {
+        $this->assertTrue(file_exists("flysystem://$path"));
+        $this->assertTrue(file_exists($this->testDir . "/$path"));
+    }
+
+    protected function assertWrapperFileNotExists($path)
+    {
+        $this->assertFalse(file_exists("flysystem://$path"));
+        $this->assertFalse(file_exists($this->testDir . "/$path"));
+    }
+
+    protected function putContent($file, $content) {
+        $len = file_put_contents("flysystem://$file", $content);
+        $this->assertSame(strlen($content), $len);
+        $this->assertFileContent($file, $content);
     }
 }
