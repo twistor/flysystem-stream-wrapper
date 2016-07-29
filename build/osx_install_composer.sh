13b2e57043e0cda57ea8837ee6fa23c12f5d8d38
Chris Leppanen <chris.leppanen@gmail.com>
2016-07-28 21:08:49 -0700

    Update composer hash.

--- build/osx_install_composer.sh
+++ build/osx_install_composer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '070854512ef404f16bac87071a6db9fd9721da1684cd4589b1196c3faf71b9a2682e2311b36a5079825e155ac7ce150d') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php -r "if (hash_file('SHA256', 'composer-setup.php') === '5af00ed7dbe47d849eca28cd44b6064d34b362ce20ba465652d72b4d121134b1') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 sudo php composer-setup.php --install-dir=/usr/local/bin/ --filename=composer
 php -r "unlink('composer-setup.php');"
