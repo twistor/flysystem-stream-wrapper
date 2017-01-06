6418b5ad2964c5418c57c6530785b2cc020260b1
Chris Leppanen <chris.leppanen@gmail.com>
2017-01-05 18:46:02 -0800

    Update OSX travis build.

--- build/osx_install_composer.sh
+++ build/osx_install_composer.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA256', 'composer-setup.php') === '5af00ed7dbe47d849eca28cd44b6064d34b362ce20ba465652d72b4d121134b1') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 sudo php composer-setup.php --install-dir=/usr/local/bin/ --filename=composer
-php -r "unlink('composer-setup.php');"
+rm composer-setup.php
